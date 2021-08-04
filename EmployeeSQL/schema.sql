e218c8fcf085a035baba2dae95ace2e74fd7aa49
Matt Carlson <mjcarlson628@gmail.com>
2021-08-04 13:43:43 -0400

    analysis 2

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -8,14 +8,15 @@ CREATE TABLE titles (
 	,title VARCHAR
 );
 
+DROP TABLE IF EXISTS employees; 
 CREATE TABLE employees (
 	emp_no INT PRIMARY KEY
 	,emp_title_id VARCHAR
-	,birth_date VARCHAR
+	,birth_date DATE
 	,first_name VARCHAR
 	,last_name VARCHAR
 	,sex VARCHAR(1)
-	,hire_date VARCHAR
+	,hire_date DATE
 	,FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
@@ -38,3 +39,17 @@ CREATE TABLE salaries (
 	,salary INT
 	,FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
+
+
+-- Data Analysis
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no, last_name, first_name, sex, salary
+FROM employees e
+JOIN salaries s
+ON e.emp_no = s.emp_no;
+
+--2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE YEAR(TO_DATE(hire_date, 'DD/MM/YYYY')) = 1986;
+
